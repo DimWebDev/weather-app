e9947ce5f1d6d69197fc64a541c08234f9ff4403
Dimitrios Liasis <dimitriosliasis@192.168.1.38>
2023-12-09 15:13:30 +0200

    proceed in BE. Defining the api paths and make first test calls

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Enable All CORS Requests for all routes
+app.use(cors());
+
+// Middleware to handle json body parsing
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Weather API"));
+
+// Weather route
+app.get('/weather', async (req, res) => {
+  const lat = 47.6062; // Hardcoded latitude for Seattle
+  const lon = -122.3321; // Hardcoded longitude for Seattle
+  const api_key = 'your_api_key'; // Replace with your actual OpenWeatherMap API key
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${api_key}`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching weather data' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
